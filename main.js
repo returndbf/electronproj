485838199a00fdbb8fa7eb7238c8551049fc508f
returndbf <3081531280@qq.com>
2021-08-03 21:57:47 +0800

    little update

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+/*
+ * @Author: dabenfeng
+ * @Date: 2021-07-31 15:09:05
+ * @LastEditTime: 2021-08-01 15:20:39
+ * @LastEditors: Please set LastEditors
+ * @Description: 主进程
+ * @FilePath: \electrondemo\main.js
+ */
+//引入相应模块
+const { electron, app, BrowserWindow, globalShortcut, Notification } = require('electron')
 // 保持一个对于 window 对象的全局引用，不然，当 JavaScript 被 GC，
 // window 会被自动地关闭
-var mainWindow = null;
+let mainWindow = null;
 //警告窗口
-function error(){
-    electron.dialog.showErrorBox('title','content')
+function error() {
+    electron.dialog.showErrorBox('title', 'content')
+}
+//系统通知
+function showNotification() {
+    new Notification({ title: 'NOTIFICATION_TITLE', body: 'NOTIFICATION_BODY' }).show()
 }
 // 当 Electron 完成了初始化并且准备创建浏览器窗口的时候
 // 这个方法就被调用
 app.on('ready', function () {
+    //注册快捷键
+    globalShortcut.register('ctrl+q', () => {
+        app.exit()
+    })
     // 创建浏览器窗口。
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: { nodeIntegration: true, contextIsolation: false }
-    }); 
+        width: 200,
+        height: 150,
+        webPreferences: { nodeIntegration: true, contextIsolation: false },
+        //禁止缩放窗口
+        resizable: false,
+        //去除边框
+        frame:false
+        
+    });
     //需要在主进程注册菜单才可使用
     require('./render/menu.js')
     // 加载应用的 index.html
--- render/menu.js
+++ render/menu.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: dabenfeng
+ * @Date: 2021-07-31 15:09:05
+ * @LastEditTime: 2021-07-31 19:12:56
+ * @LastEditors: Please set LastEditors
+ * @Description: 自定义菜单栏
+ * @FilePath: \electrondemo\render\menu.js
+ */
 
 const {Menu} = require('electron');
 let template =[{
